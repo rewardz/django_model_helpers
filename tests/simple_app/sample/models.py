ecfc4ea00638346fca3cca24b99746a2a7d7307e
Ramast Magdy <ramast.com@gmail.com>
2016-01-31 07:25:33 +0800

    Removed experimental code

--- tests/simple_app/sample/models.py
+++ tests/simple_app/sample/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from model_helpers import cached_model_property, KeyValueField, SimpleGenericForeignKey
+from model_helpers import cached_model_property, KeyValueField
 
 
 class Team(models.Model):
@@ -26,26 +26,3 @@ class Team(models.Model):
         self.counter += 1
         return self.counter
 
-
-class ModelX(models.Model):
-    link = SimpleGenericForeignKey(null=True)
-
-
-@SimpleGenericForeignKey.register_generic_model(index=1)
-@python_2_unicode_compatible
-class ModelA(models.Model):
-
-    name_a = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name_a
-
-
-@SimpleGenericForeignKey.register_generic_model(index=2)
-@python_2_unicode_compatible
-class ModelB(models.Model):
-
-    name_b = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name_b
